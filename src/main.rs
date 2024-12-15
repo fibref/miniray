53764a22216b3046dd00759e3432cfa68b4b37f0
frank <supermouse_233@outlook.com>
2024-12-15 17:32:19 +0800

    be able to output an image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::fs::File;
+
+
 fn main() {
     println!("Hello, world!");
+
+    let out_file = File::create("output.png").expect("Unable to create file");
+    let mut encoder = png::Encoder::new(out_file, 800, 600);
+    encoder.set_color(png::ColorType::Rgb);
+    encoder.set_depth(png::BitDepth::Eight);
+
+    let mut writer = encoder.write_header().expect("Unable to write header");
+    
+    let data = unsafe { Box::<[u8; 800 * 600 * 3]>::new_uninit().assume_init() };
+    
+    writer.write_image_data(data.as_slice()).expect("Unable to write image data");
 }
