cf87cbb3751d699c4f7fc302f8b9e06215a1610d
frank <supermouse_233@outlook.com>
2024-12-27 20:05:14 +0800

    Texture: use fields to store width and height instead of generics

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,20 @@ use texture::Texture;
 mod vec3;
 mod texture;
 
+const IMG_WIDTH: u32 = 800;
+const IMG_HEIGHT: u32 = 600;
+
 fn main() {
     println!("Hello, world!");
 
     let out_file = File::create("output.png").expect("Unable to create file");
-    let mut encoder = png::Encoder::new(out_file, 800, 600);
+    let mut encoder = png::Encoder::new(out_file, IMG_WIDTH, IMG_HEIGHT);
     encoder.set_color(png::ColorType::Rgb);
     encoder.set_depth(png::BitDepth::Eight);
 
     let mut writer = encoder.write_header().expect("Unable to write header");
     
-    let data: Texture<800, 600> = Texture::new();
+    let data: Texture = Texture::new(IMG_WIDTH, IMG_HEIGHT);
     
     writer.write_image_data(data.rgb_buffer().as_slice()).expect("Unable to write image data");
 }
--- src/texture.rs
+++ src/texture.rs
@@ -1,16 +1,22 @@
-use crate::vec3;
+use crate::vec3::Vec3;
 
-pub struct Texture<const W: usize, const H: usize> {
-    buffer: Vec<vec3::Vec3>
+pub struct Texture {
+    width: u32,
+    height: u32,
+    buffer: Vec<Vec3>
 }
 
-impl<const W: usize, const H: usize> Texture<W, H> {
-    pub fn new() -> Self {
-        Self { buffer: vec![vec3::Vec3(0.0, 0.0, 0.0); W * H] }
+impl Texture {
+    pub fn new(width: u32, height: u32) -> Self {
+        Self {
+            width: width,
+            height: height,
+            buffer: vec![Vec3(0.0, 0.0, 0.0); (width * height) as usize]
+        }
     }
 
     pub fn rgb_buffer(&self) -> Vec<u8> {
-        let mut buf: Vec<u8> = Vec::with_capacity(W * H * 3);
+        let mut buf: Vec<u8> = Vec::with_capacity((self.width * self.height * 3) as usize);
         for color in &self.buffer {
             buf.push((color.0 * 255.0) as u8);
             buf.push((color.1 * 255.0) as u8);
